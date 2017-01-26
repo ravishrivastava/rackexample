2d86916782d473d2f4e00b5926f63ae936a0b8dc
ravi <rshri@gmail.com>
2017-01-26 12:15:02 +0530

    cleaned up users_application code

--- users_application.rb
+++ users_application.rb
@@ -1,23 +1,30 @@
+
+require_relative 'application_helpers'
 class UsersApplication
+  include ApplicationHelpers
   def call(env)
     request = Rack::Request.new(env)
     response = Rack::Response.new
     response.headers["Content-Type"] = "application/json" 
-   if request.path_info == "" 
+    case request.path_info
+    when  ""
       response.write(JSON.generate(Database.users))
-    elsif request.path_info =~ %r{/\d+}
-      id = request.path_info.split('/').last.to_i 
-      user = Database.users[id]
-      if user.nil?
-        response.status = 404
-        response.write("No user with id #{id}")
-      else     
-        response.write(JSON.generate(user))
-      end
+    when  %r{/\d+}
+      get_a_user(request,response)
     else
-      response.status = 404
-      response.write("Nothing here!")
+      error(response,"Nothing here!",404)
     end
     response.finish
   end
+
+  def get_a_user(request,response)
+      id = request.path_info.split('/').last.to_i 
+      user = Database.users[id]
+      if user.nil?
+	error(response,"No user with id #{id}",404)
+      else     
+	response_with_object(response,user)
+      end
+  end
+
 end
